fc4a42993eca369ff586c24854e0025ddfc54c48
NadoraLuka <69468234+NadoraLuka@users.noreply.github.com>
2020-10-14 12:40:56 -0400

    picture added to assets

--- app.js
+++ app.js
@@ -10,70 +10,53 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
 const questionPrompt = [
-
-    {
-        type: "list",
-        name: "begin",
-        message: "Are you hiring new employee?",
-        choices: [
-            "Yes",
-            "No"
-        ]
-    },
-    {
-        type: "input",
-        message: "What is your name?",
-        name: "name"
-
-    },
-    {
-        type: "input",
-        message: "What is your email address?",
-        name: "email",
-    },
-    {
-        type: "input",
-        message: "What is your employee ID number?",
-        name: "number",
-    },
-    {
-        type: "list",
-        message: "What is your role?",
-        name: "role",
-        choices: [
-            "Engineer",
-            "Intern",
-            "Manager",
-            "Other"
-        ],
-    },
-    {
-        type: "input",
-        message: "What is your GitHub username?",
-        name: "github",
-
-    },
-    {
-        type: "input",
-        message: "Where do you attend school?",
-        name: "school",
-    },
-    {
-        type: "input",
-        message: "What is your office number?",
-        name: "officeNumber",
-    }
-]
+  {
+    type: "list",
+    name: "begin",
+    message: "Are you hiring new employee?",
+    choices: ["Yes", "No"],
+  },
+  {
+    type: "input",
+    message: "What is your name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your employee ID number?",
+    name: "number",
+  },
+  {
+    type: "list",
+    message: "What is your role?",
+    name: "role",
+    choices: ["Engineer", "Intern", "Manager", "Other"],
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "Where do you attend school?",
+    name: "school",
+  },
+  {
+    type: "input",
+    message: "What is your office number?",
+    name: "officeNumber",
+  },
+];
 const team = [];
 
 function teamBuilder() {
-inquirer.prompt(questionPrompt);
+  inquirer.prompt(questionPrompt);
 }
 teamBuilder();
-
-
--- test/Employee.test.js
+++ test/Employee.test.js
@@ -2,11 +2,11 @@ const Employee = require("../lib/Employee");
 
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
-  expect(typeof(e)).toBe("object");
+  expect(typeof e).toBe("object");
 });
 
 test("Can set name via constructor arguments", () => {
-  const name = "Alice";
+  const name = "Velia";
   const e = new Employee(name);
   expect(e.name).toBe(name);
 });
@@ -24,7 +24,7 @@ test("Can set email via constructor argument", () => {
 });
 
 test("Can get name via getName()", () => {
-  const testValue = "Alice";
+  const testValue = "Velia";
   const e = new Employee(testValue);
   expect(e.getName()).toBe(testValue);
 });
@@ -41,8 +41,8 @@ test("Can get email via getEmail()", () => {
   expect(e.getEmail()).toBe(testValue);
 });
 
-test("getRole() should return \"Employee\"", () => {
+test('getRole() should return "Employee"', () => {
   const testValue = "Employee";
-  const e = new Employee("Alice", 1, "test@test.com");
+  const e = new Employee("Velia", 1, "test@test.com");
   expect(e.getRole()).toBe(testValue);
 });
